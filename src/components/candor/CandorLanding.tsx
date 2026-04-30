c1740a031522f3298a01571925d91d549c7d61fc
Divyom Sharma <divyomsharma180307@gmail.com>
2026-04-30 18:59:10 +0530

    fix: dynamic import AsciiBackground with ssr:false to prevent prerender crash

--- src/components/candor/CandorLanding.tsx
+++ src/components/candor/CandorLanding.tsx
@@ -4,7 +4,9 @@ import { useEffect } from "react";
 import dynamic from "next/dynamic";
 import Index from "@/landing/Index";
 
-const AsciiBackground = dynamic(() => import("@/components/AsciiBackground"), { ssr: false });
+const AsciiBackground = dynamic(() => import("@/components/AsciiBackground"), {
+  ssr: false,
+});
 
 export default function CandorLanding() {
   useEffect(() => {
